db7292228ccaafbf2ab936304671c003e2248478
Paul Ashour <paul.ashour@gmail.com>
2019-06-07 22:40:20 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+function logDriverNames(dArr) {
+  dArr.forEach(d => {
+    console.log(d.name);
+  });
+}
+
+function logDriversByHometown(dArr, loc) {
+  dArr.filter( d => { return d.hometown === loc}).forEach(d => {
+    console.log(d.name);
+  });
+}
+
+
+function driversByRevenue(dArr) {
+  return dArr.slice().sort( function (d1, d2) {return d1.revenue - d2.revenue })
+}
+
+function driversByName(dArr) {
+  return dArr.slice().sort(function (d1, d2) {
+    return d1.name.localeCompare(d2.name);
+  })
+}
+
+function totalRevenue(dArr) {
+  return dArr.reduce(function (a, e, i, r) {
+    return a + e.revenue;
+  }, 0);
+}
+
+function averageRevenue(dArr) {
+  return totalRevenue(dArr) / dArr.length;
+}
